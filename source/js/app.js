64a15287f780b8c2ef73599777d3a8df66c602fd
Nikita <nekmalienko2001@gmail.com>
2021-01-31 21:09:39 +0200

    fancybox

--- source/js/app.js
+++ source/js/app.js
@@ -15,18 +15,18 @@ $(document).ready(function () {
         menuModal.toggleClass('show');
     });
 
-    $(document).keydown(function (e) {
-        if (e.keyCode === 27) {
-            e.stopPropagation();
-
-            $(navToggle).toggleClass('active');
-            $('.header__logo--first').toggleClass('hide');
-            $('.header__logo--second').toggleClass('show');
-            $('.header__right').fadeToggle();
-            $('body').toggleClass('lock');
-            menuModal.toggleClass('show');
-        }
-    });
+    // $(document).keydown(function (e) {
+    //     if (e.keyCode === 27) {
+    //         e.stopPropagation();
+    //
+    //         $(navToggle).toggleClass('active');
+    //         $('.header__logo--first').toggleClass('hide');
+    //         $('.header__logo--second').toggleClass('show');
+    //         $('.header__right').fadeToggle();
+    //         $('body').toggleClass('lock');
+    //         menuModal.toggleClass('show');
+    //     }
+    // });
 
     /* Drop down */
     $('.action__link-dropdown').click(function (e) {
